b8aa5490d735199b41f04638df72fcc3b395e998
alexPV10 <129249120+alexPV10@users.noreply.github.com>
2023-09-02 01:41:42 -0600

    Java

---
+++ ControlNotas/src/controlnotas/ControlNotas.java
@@ -0,0 +1,12 @@
+/*
+TAREA: programa que permita consultar las notas del ciclo actual
+ */
+package controlnotas;
+
+public class ControlNotas {
+
+    public static void main(String[] args) {
+        
+    }
+    
+}
